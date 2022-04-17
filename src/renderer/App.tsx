aaf5462bddea0c128c3f85c2366abbdb6893fc13
Khalil Selyan <khalilselyan2@gmail.com>
2022-04-17 23:00:38 +0300

    MainPage Setup

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,12 +1,69 @@
 import React from "react";
 import MainPage from "./MainPage";
 import SideBar from "./SideBar";
-
+import Logger from "./view/Logger";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { logState, colorSchemeState, pageState } from "./atoms";
+import "./app.css";
+import appRuntime from "./modules/appRuntime";
+import closeIcon from "/closeIcon.svg";
 const App = () => {
+  const logged = useRecoilValue(logState);
+  const colorScheme = useRecoilValue(colorSchemeState);
+
+  const [page, setPage] = useRecoilState(pageState);
+
+  React.useEffect(() =>
+    appRuntime.subscribe("pageOpen", (data) => {
+      setPage(data);
+    })
+  );
+
   return (
-    <div id="root" className="flex flex-row">
-      <SideBar />
-      <MainPage />
+    <div
+      id="root"
+      className={` ${
+        colorScheme === "light" ||
+        colorScheme === "DanahPurple" ||
+        colorScheme === "PekiDawn" ||
+        colorScheme === "PeacockGreen" ||
+        colorScheme === "ocean"
+          ? "app-light-bg"
+          : colorScheme === "dark" ||
+            colorScheme === "MidnightDark" ||
+            colorScheme === "BlackYellow"
+          ? "app-dark-bg"
+          : ""
+      }  flex flex-row w-full rounded-[35px] font-baloo`}
+    >
+      {!logged && <Logger />}
+      {logged && (
+        <div className="">
+          <div className="sidebar">
+            <SideBar />
+          </div>
+          <div className="pt-8 pb-4 ml-11 main min-h-screen">
+            {/* <span>{msg}</span> */}
+            <MainPage />
+          </div>
+        </div>
+      )}
+      <div
+        onClick={() => {
+          appRuntime.send("close-window", {});
+        }}
+        className={`absolute top-2 right-4 m-2 cursor-pointer ${
+          colorScheme === "light"
+            ? "bg-gray-400 hover:bg-gray-600"
+            : colorScheme === "dark"
+            ? "bg-gray-500 hover:bg-gray-300"
+            : colorScheme === "ocean"
+            ? "bg-blue-500 hover:bg-blue-600"
+            : ""
+        }  rounded-full p-1 transition-colors duration-200 transform`}
+      >
+        <img src={closeIcon} alt="close" className="h-4 w-4" />
+      </div>
     </div>
   );
 };
--- src/renderer/MainPage.tsx
+++ src/renderer/MainPage.tsx
@@ -1,12 +1,39 @@
 import React from "react";
+import { useRecoilValue } from "recoil";
+import { pageState } from "./atoms";
+import Gallery from "./view/Gallery";
+import Profile from "./view/Profile";
+import Settings from "./view/Settings";
+import About from "./view/About";
+import PopupImageView from "./components/PopupImageView";
+import SearchBar from "./components/SearchBar";
+import Team from "./view/Team";
 
 function MainPage() {
+  const page = useRecoilValue(pageState);
+  const [search, setSearch] = React.useState("");
   return (
-    <div className="flex flex-col">
-      <span>Gallery</span>
-      <span>Profile</span>
-      <span>Settings</span>
-      <span>About</span>
+    <div className="flex flex-col overflow-y-scroll">
+      {page === "Explore" && (
+        <div>
+          <div className="ml-3">
+            <SearchBar placeholder="Search for images" setSearch={setSearch} />
+          </div>
+          <Gallery />
+        </div>
+      )}
+      {page === "Profile" && (
+        <div>
+          <div className="ml-3">
+            <SearchBar placeholder="Search for images" setSearch={setSearch} />
+          </div>
+          <Profile />
+        </div>
+      )}
+      {page === "Settings" && <Settings />}
+      {page === "About" && <About />}
+      {page === "Team" && <Team />}
+      <PopupImageView />
     </div>
   );
 }
