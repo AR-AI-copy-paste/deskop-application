67cae8d4b0e4ee3b325b4147bbddb9f61cef38e9
Khalil Selyan <khalilselyan2@gmail.com>
2022-05-05 16:00:05 +0300

    Config update

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,5 +1,10 @@
 /** @type { import("snowpack").SnowpackUserConfig } */
 module.exports = {
+  env: {
+    REACT_APP_SUPABASE_URL: "https://cnllwvkqmwetcbmpxpjn.supabase.co",
+    REACT_APP_SUPABASE_ANON_KEY:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNubGx3dmtxbXdldGNibXB4cGpuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NDg2NTE3OTYsImV4cCI6MTk2NDIyNzc5Nn0.apzDrOPTbv9blQPCUTY39Lh2Dx9iQ3KtdzqQIIPua9w",
+  },
   mount: {
     src: "/_dist",
     public: "/",
